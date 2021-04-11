0185c91201f5cbd2f6490d38db1a8e646438eb7a
Dima Gusonka <gusonkad@gmail.com>
2021-04-11 09:07:13 +0300

    Add third task to tasks_main.js

--- tasks_main.js
+++ tasks_main.js
@@ -7,10 +7,21 @@ const getUserNames = users => users.map(user => user.name);
  // [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
 // ===================================
 // task_02.js
+// Получить массив объектов пользователей по цвету глаз (поле eyeColor).
+
+
+const getUsersWithEyeColor = (users, color) => users.filter(user => user.eyeColor === color);
+console.table(getUsersWithEyeColor(users, 'blue')); 
+ // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
+
+
+// ===================================
+// task_03.js  
  //  Получить массив имен пользователей по полу (поле gender).
  const getUsersWithGender = (users, gender) => users.filter(user => user.gender === gender).map(user => user.name);
   console.table(getUsersWithGender(users, 'male')); 
   // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
 
+
 // ===================================
-// task_03.js  
+// task_04.js 
