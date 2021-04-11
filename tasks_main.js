e97e7955ae2df67a476c47016a35896c1ad27c48
Dima Gusonka <gusonkad@gmail.com>
2021-04-11 08:56:32 +0300

    Add second task to tasks_main.js

--- tasks_main.js
+++ tasks_main.js
@@ -2,8 +2,15 @@ import users from "./users.js";
 
 // task_01.js
 // Получить массив имен всех пользователей (поле name).
-
 const getUserNames = users => users.map(user => user.name);
- console.log(getUserNames(users));
+ console.table(getUserNames(users));
  // [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+// ===================================
+// task_02.js
+ //  Получить массив имен пользователей по полу (поле gender).
+ const getUsersWithGender = (users, gender) => users.filter(user => user.gender === gender).map(user => user.name);
+  console.table(getUsersWithGender(users, 'male')); 
+  // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
 
+// ===================================
+// task_03.js  
